e085eb5b240d7f79c694887718af3abff97b3bc4
khaledahmed <khaled.ahmed333774@gmail.com>
2025-03-09 21:13:47 +0200

    add update profile method

--- Teamspace/Teamspace/Controllers/ProfileController.cs
+++ Teamspace/Teamspace/Controllers/ProfileController.cs
@@ -38,6 +38,41 @@ namespace Teamspace.Controllers
             }
             return BadRequest("Invalid Role please ensure you select a valid role :)");
         }
-        
+        [HttpPut("[action]")]
+        public async Task<IActionResult> Update([FromQuery] int role, [FromQuery] int id, [FromForm] Profile profile)
+        {
+            if (role == 0)
+            {
+                var student = await _db.Students.SingleOrDefaultAsync(s => s.Id == id);
+                if (student == null)
+                    return NotFound("This Profile doesn't exist :(");
+
+                //student.Name = account.Name;
+                student.Password = profile.Password;
+                student.PhoneNumber = profile.PhoneNumber;
+                //student.NationalId = account.NationalId;
+                //student.Email = account.Email;
+                student.Gender = profile.Gender;
+                //student.Year = account.Year;
+                //student.DepartmentId = account.DepartmentId;
+
+                await _db.SaveChangesAsync();
+                return Ok(new { Message = "Profile Updated Successfully :)", student });
+            }
+            else if (role == 1)
+            {
+                var staff = await _db.Staffs.SingleOrDefaultAsync(s => s.Id == id);
+                if (staff == null)
+                    return NotFound("This Profile doesn't exist :(");
+
+
+                staff.Password = profile.Password;
+                staff.PhoneNumber = profile.PhoneNumber;
+                staff.Gender = profile.Gender;
+                await _db.SaveChangesAsync();
+                return Ok(new { Message = "Profile Updated Successfully :)", staff });
+            }
+            return BadRequest("Invalid Role please ensure you select a valid role :)");
+        }
     }
 }
