e7461196535860b31e4fc40a595f0aa386c1ff98
khaledahmed <khaled.ahmed333774@gmail.com>
2025-03-09 21:12:52 +0200

    add get profile by id method

---
+++ Teamspace/Teamspace/Controllers/ProfileController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Teamspace.Configurations;
+using Teamspace.DTO;
+using Teamspace.Models;
+
+namespace Teamspace.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfileController : ControllerBase
+    {
+        AppDbContext _db;
+        public ProfileController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet("[action]")]
+        public async Task<IActionResult> GetById(int role, int id)
+        {
+            if(role == 0)
+            {
+                var student = await _db.Students.SingleOrDefaultAsync(s => s.Id == id);
+                if(student == null)
+                    return NotFound("This Profile doesn't exist :(");
+
+                return Ok(student);
+            }
+            else if(role == 1)
+            {
+                var staff = await _db.Staffs.SingleOrDefaultAsync(s => s.Id == id);
+                if (staff == null)
+                    return NotFound("This Profile doesn't exist :(");
+
+                return Ok(staff);
+            }
+            return BadRequest("Invalid Role please ensure you select a valid role :)");
+        }
+        
+    }
+}
---
+++ Teamspace/Teamspace/DTO/Profile.cs
@@ -0,0 +1,11 @@
+﻿namespace Teamspace.DTO
+{
+    public class Profile
+    {
+        public bool Gender { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+
+        // image
+    }
+}
