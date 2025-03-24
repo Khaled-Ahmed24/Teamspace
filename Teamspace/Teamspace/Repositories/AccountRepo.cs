0aeb5dd7ecd604da9a4d24427dfde89005c61bb5
khaledahmed <khaled.ahmed333774@gmail.com>
2025-03-24 23:06:09 +0200

    Add Account Repository :)

---
+++ Teamspace/Teamspace/Repositories/AccountRepo.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Teamspace.Configurations;
+using Teamspace.Models;
+
+namespace Teamspace.Repositories
+{
+    public class AccountRepo
+    {
+        private readonly AppDbContext _db;
+        public AccountRepo(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public List<Student> GetAllStudents()
+        {
+            var students = _db.Students.ToList();
+            return students;
+        }
+        
+        public List<Staff> GetAllStaffs()
+        {
+            var staffs = _db.Staffs.ToList();
+            return staffs;
+        }
+    }
+}
